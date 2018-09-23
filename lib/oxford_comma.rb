2258ce32838e49050b8889585586a8954893e239
Kevin J Gomez <kjgmez1@gmail.com>
2018-09-23 04:28:57 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  x = array.count
+  if x < 2 
+    array.join
+    elsif x == 2
+      array.join(" and ")
+    else 
+      y = array.last 
+      array.pop
+      z = array.join(", ")
+      z << (", and #{y.to_s}")
+    end
 end
